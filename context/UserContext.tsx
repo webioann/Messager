68888ff8f96775fa53b0a224c6d8a1a301da6173
Webioann <ioannn7777@gmail.com>
2023-12-10 09:07:10 +0200

    prop function setCurrentUser on UserContext

--- context/UserContext.tsx
+++ context/UserContext.tsx
@@ -1,12 +1,16 @@
-import React, { createContext, useEffect, useState, ReactNode } from "react";
+import React, { createContext, useEffect, useState, ReactNode, SetStateAction } from "react";
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
 import { UserType, currentUserType } from "../Types/users_types";
 
 type childrenType = {
     children: ReactNode[] | ReactNode 
 }
+type authContextValueProps = {
+    currentUser: currentUserType | null
+    setCurrentUser: React.Dispatch<SetStateAction<currentUserType | null>>
+}
 
-export const UserContext = createContext<currentUserType | null>(null);
+export const UserContext = createContext<authContextValueProps | null>(null);
 
 export const USER_CONTEXT_PROVIDER: React.FC<childrenType> = ({ children }) => {
     const [currentUser, setCurrentUser] = useState<currentUserType | null>(null);
@@ -74,7 +78,7 @@ export const USER_CONTEXT_PROVIDER: React.FC<childrenType> = ({ children }) => {
     // TODO:
     console.log(`AUTH_STATE_CONTEXT_USER --->`, currentUser)
     return (
-        <UserContext.Provider value={currentUser}>
+        <UserContext.Provider value={{currentUser, setCurrentUser}}>
             {children}
         </UserContext.Provider>
     );
