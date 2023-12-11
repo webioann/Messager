d8be9b7762ab4d46ed1f5b0dc8ebea8f79577169
Webioann <ioannn7777@gmail.com>
2023-12-11 11:40:06 +0200

    testing image uploader

--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useContext } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, Alert, Keyboard } from 'react-native'
 import { UserContext } from '../context/UserContext';
+import { ColorSchemeContext } from '../context/ColorSchemeContext';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import ImageUploader from './ImageUploader';
 import UploadImageInStorage from './UploadImageInStorage';
-import { COLORS, SIZES, G } from '../constants/SIZES';
+import { SIZES, G } from '../constants/SIZES';
 import firestore from '@react-native-firebase/firestore';
 import {  messageType } from '../Types/chats_types';
 
@@ -16,23 +16,25 @@ type RoomProp = {
 const MessageInput: React.FC<RoomProp> = ({ room, senderID }) => {
     const [message, setMessage] = useState('')
     const [image, setImage] = useState<string | undefined>(undefined)
+    const { COLORS } = useContext(ColorSchemeContext);
+
     const user = useContext(UserContext)
 
     const addDataInFirestore = async () => {
         try {
             if(message.length > 0 ) {
-                let newMessage: messageType = {
+                let newMessage: messageType = {// <--- fill message object
                     text: message,
                     senderID: senderID,
                     createdAt: Date.now(),
                     reviewed: false,
                     files: []
                 } 
-                await firestore().collection('CHAT_ROOM_DB').doc(room).update({
+                await firestore().collection('CHAT_ROOM_DB').doc(room).update({// <--- push new message in array
                     messages: firestore.FieldValue.arrayUnion(newMessage)
                 })
             }
-            if(message.length == 0 && image) {
+            if(message.length == 0 && image) {// <--- if input empty but need send image
                 let newMessage: messageType = {
                     text: '',
                     senderID: senderID,
@@ -56,34 +58,32 @@ const MessageInput: React.FC<RoomProp> = ({ room, senderID }) => {
 
     return (
         <View style={styles.wrapper}>
-            <View style={styles.tools}>
+            <View style={[styles.tools, {borderColor: COLORS.tint, backgroundColor: COLORS.main}]}>
             <TouchableOpacity 
                     style={{transform: [{rotate: '315deg'}]}}
                     onPress={() => console.log('click on paperclip icon')}>
-                    <Icon name='paperclip' color={COLORS.ACCENT} size={24}/>    
+                    <Icon name='paperclip' color={COLORS.orange} size={24}/>    
                 </TouchableOpacity>
                 <TextInput 
                     value={message}
                     onChangeText={setMessage}
                     placeholder='Type a message'
-                    placeholderTextColor={COLORS.GREY}
-                    cursorColor={COLORS.LIGHT}
+                    placeholderTextColor={COLORS.tint}
+                    cursorColor={COLORS.color}
                     multiline={true}
-                    style={{flex: 1, color: COLORS.LIGHT, fontSize: 18}}/>
+                    style={{flex: 1, color: COLORS.color, fontSize: 18}}/>
                 <TouchableOpacity 
                     style={{transform: [{rotate: '330deg'}]}} 
                     onPress={addDataInFirestore}> 
-                    <Icon name='send' color={COLORS.BLUE} size={24}/>    
+                    <Icon name='send' color={COLORS.blue} size={24}/>    
                 </TouchableOpacity>
 
                 <TouchableOpacity 
                     onPress={() => console.log('click on camera icon')}>
-                    <Icon name='camera-outline' color={COLORS.ACCENT} size={24}/>    
+                    <Icon name='camera-outline' color={COLORS.orange} size={24}/>    
                 </TouchableOpacity>
-                <UploadImageInStorage getImageURL={setImage} uniqueName='NAME'  color={COLORS.ACCENT} size={24}/>
-                {/* <ImageUploader setFilePath={setImage} color={COLORS.ACCENT} size={24}/> */}
+                <UploadImageInStorage getImageURL={setImage} uniqueName='NAME'  color={COLORS.orange} size={24}/>
             </View>
-            {/* === microphone === */}
         </View>
     )
 }
@@ -103,16 +103,7 @@ const styles = StyleSheet.create({
         marginRight: 10,
         gap: 10,
         paddingHorizontal: 14,
-        borderColor: COLORS.GREY,
         borderStyle: 'solid',
         borderWidth: 0.5,
-    },
-    microphone: {
-        width: SIZES.BIG,
-        height: SIZES.BIG,
-        borderRadius: SIZES.BIG / 2,
-        backgroundColor: COLORS.ACCENT,
-        justifyContent: 'center',
-        alignItems: 'center'
     },
 });
