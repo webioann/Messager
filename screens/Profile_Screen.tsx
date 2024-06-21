84dfb10d25e27b5dca8723a43fd7437047695b57
Webioann <ioannn7777@gmail.com>
2024-06-21 08:37:42 +0300

    empty commit

--- screens/Profile_Screen.tsx
+++ screens/Profile_Screen.tsx
@@ -56,6 +56,7 @@ const Profile_Screen = () => {
         }
         else return
     }
+    
     const genderFieldValidation = (previousGender: GenderType) => {
         let validGender = previousGender
         // changes were not yet
